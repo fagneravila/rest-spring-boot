1d6683ffff7ca0ec97f2641ab7a98b768c4812c1
fagneravila <fagner_avila99@hotmail.com>
2025-02-20 01:25:10 -0300

    first step

---
+++ rest-spring-boot/src/main/java/br/com/avila/rest_spring_boot/model/Greenting.java
@@ -0,0 +1,4 @@
+package br.com.avila.rest_spring_boot.model;
+
+public record Greenting(long id, String content) {
+}
---
+++ rest-spring-boot/src/test/java/br/com/avila/rest_spring_boot/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avila.rest_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
